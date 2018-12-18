090f962060dd816ae779451912393f0054bda05e
Sunsp <diudiu2015@users.noreply.github.com>
2018-12-18 17:32:25 +0800

    text 用法

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'welcon to flutter',
       theme: new ThemeData(
         primaryColor: Colors.red,
-        canvasColor: Colors.grey,
-        accentColor: Colors.yellow,
-        hintColor: Colors.blue,
+//        canvasColor: Colors.grey,
+//        accentColor: Colors.yellow,
+//        hintColor: Colors.blue,
       ),
       routes: _routes,
 //      home: new Scaffold(
@@ -48,19 +48,22 @@ class myHomePage extends StatelessWidget{
           appBar: new AppBar(
             title: new Text('dsdd'),
           ),
-          body: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              new BottomNavigationBarItem(
-                  icon: Icon(Icons.home), title: Text('home')),
-              new BottomNavigationBarItem(
-                  icon: Icon(Icons.add), title: Text('add')),
-              new BottomNavigationBarItem(
-                  icon: Icon(Icons.add), title: Text('add')),
-            ],
-            onTap: (index) {
-              print('log 123');
-            },
-          ));
+      //softWrap 为 false 时候，说明不允许换行，这个时候相当于 maxLines 为 1.
+          body:  Text(
+              r'$$5633我的生活3',
+
+            style: new TextStyle(fontSize: 20.0,color: Colors.teal[500],
+                decoration: TextDecoration.underline,decorationColor:Colors.blue,fontWeight:FontWeight.w800,
+
+            ),
+//            softWrap: true
+            textScaleFactor:1.0,
+            overflow: TextOverflow.clip,
+//            maxLines: 3,
+            textAlign: TextAlign.left,
+              semanticsLabel:'Text semanticsLabel'
+          ),
+    );
   }
 }
 
