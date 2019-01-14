0ce282b9d73b0336cf8ef4c319c9fdd3f81a7fe8
Sunsp <diudiu2015@users.noreply.github.com>
2019-01-14 10:07:55 +0800

    tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  final Map<String, WidgetBuilder> _routes = <String, WidgetBuilder>{
-    Navigator.defaultRouteName: (context) => new myHomePage(),
-  };
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
-      title: 'welcon to flutter',
       theme: new ThemeData(
-        primaryColor: Colors.red,
-//        canvasColor: Colors.grey,
-//        accentColor: Colors.yellow,
-//        hintColor: Colors.blue,
+        // 设置主题颜色
+          primaryColor: const Color(0xFF63CA6C),
       ),
-      routes: _routes,
-//      home: new Scaffold(
-//          appBar: new AppBar(
-//            title: new Text('dsdd'),
-//          ),
-//          body: BottomNavigationBar(
-//            items: <BottomNavigationBarItem>[
-//              new BottomNavigationBarItem(
-//                  icon: Icon(Icons.home), title: Text('home')),
-//              new BottomNavigationBarItem(
-//                  icon: Icon(Icons.add), title: Text('add')),
-//              new BottomNavigationBarItem(
-//                  icon: Icon(Icons.add), title: Text('add')),
-//            ],
-//            onTap: (index) {
-//              print('log 123');
-//            },
-//          )),
-    );
-  }
-}
-class myHomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return
-    new Scaffold(
-          appBar: new AppBar(
-            title: new Text('dsdd'),
-          ),
-      //softWrap 为 false 时候，说明不允许换行，这个时候相当于 maxLines 为 1.
-          body: new ListView(
-            children: <Widget>[
-             new Image.asset('images/adv_mngcente_bidding.png'),
-              new Divider(),
-              new Image(image: new AssetImage('images/adv_mngcente_bidding.png')),
-             new Divider(),
-//              普通网络加载
-              new Image.network('http://img03.tooopen.com/uploadfile/downs/images/20110714/sy_20110714135215645030.jpg'),
-//           从缓存加载图片
-            new CachedNetworkImage(
-                imageUrl: 'http://img03.tooopen.com/uploadfile/downs/images/20110714/sy_20110714135215645030.jpg',
-              placeholder: new CircularProgressIndicator(),
-              errorWidget: new Icon(Icons.error),
-            ),
-//              淡入动画加载图片
-            new FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: 'http://img03.tooopen.com/uploadfile/downs/images/20110714/sy_20110714135215645030.jpg')
-            ],
-          )
-    );
-  }
-}
+      home: new Scaffold(
+        // 设置App顶部的AppBar
+        appBar: new AppBar(
+          // AppBar的标题
+            title: new Text('ddd',
+                // 标题文本的颜色
+                style: new TextStyle(color: Colors.white)),
+            // AppBar上的图标的颜色
+            iconTheme: new IconThemeData(color: Colors.white)
+        ),
+        // 页面底部的导航栏
+        bottomNavigationBar: new CupertinoTabBar(
+          items: <BottomNavigationBarItem>[
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.add),
+                title: Text('ddd')),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.add),
+                title: Text('ddd')),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.add),
+                title: Text('ddd')),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.add),
+                title: Text('ddd')),
+          ],
+          currentIndex: 0,
+          // 底部Tab的点击事件处理
+          onTap: (index) {
 
+          },
+        ),
+        // 侧滑菜单，这里的MyDrawer是自定义的Widget
+
+      ),
+    );
+  }
+
+}
